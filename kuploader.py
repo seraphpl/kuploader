306a20cababaa12dc014d1937e0d52aac72332d6
seraphpl <seraphpl@kaist.ac.kr>
2014-04-26 02:07:39 +0900

    Change to upload service

--- kuploader.py
+++ kuploader.py
@@ -1,86 +1,58 @@
 import os
 import urllib
+import webapp2
 
 from google.appengine.api import users
 from google.appengine.ext import ndb
-
-import jinja2
-import webapp2
-
-
-JINJA_ENVIRONMENT = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
-    extensions=['jinja2.ext.autoescape'],
-    autoescape=True)
-
-DEFAULT_GUESTBOOK_NAME = 'default_guestbook'
+from google.appengine.ext import blobstore
+from google.appengine.ext.webapp import blobstore_handlers
 
 
-# We set a parent key on the 'Greetings' to ensure that they are all in the same
-# entity group. Queries across the single entity group will be consistent.
-# However, the write rate should be limited to ~1/second.
-
-def guestbook_key(guestbook_name=DEFAULT_GUESTBOOK_NAME):
-    """Constructs a Datastore key for a Guestbook entity with guestbook_name."""
-    return ndb.Key('Guestbook', guestbook_name)
+class UserFile(ndb.Model):
+    user = ndb.StringProperty()
+    file_name = ndb.StringProperty()
+    blob_key = ndb.BlobKeyProperty()
 
 
-class Greeting(ndb.Model):
-    """Models an individual Guestbook entry with author, content, and date."""
-    author = ndb.UserProperty()
-    content = ndb.StringProperty(indexed=False)
-    date = ndb.DateTimeProperty(auto_now_add=True)
-
-
-class MainPage(webapp2.RequestHandler):
-
+class MainHandler(webapp2.RequestHandler):
     def get(self):
-        guestbook_name = self.request.get('guestbook_name',
-                                          DEFAULT_GUESTBOOK_NAME)
-        greetings_query = Greeting.query(
-            ancestor=guestbook_key(guestbook_name)).order(-Greeting.date)
-        greetings = greetings_query.fetch(10)
+        files = UserFile.query(
+            UserFile.user == users.get_current_user().user_id()
+        ).fetch()
+        upload_url = blobstore.create_upload_url('/upload')
+        self.response.out.write('<html><body>')
+        self.response.out.write('%s<br><a href="%s">Logout</a><br>' %
+                                (users.get_current_user().nickname(),
+                                users.create_logout_url(self.request.uri)))
+        self.response.out.write('<form action="%s" method="POST" enctype="multipart/form-data">' % upload_url)
+        self.response.out.write("""Upload File: <input type="file" name="file"><br> <input type="submit"
+            name="submit" value="Submit"> </form>""")
+        for file in files:
+            self.response.write('<a href="%s">%s</a><br>' % 
+                                ('/serve/%s' % file.blob_key, file.file_name))
+        self.response.out.write('</body></html>')
 
-        if users.get_current_user():
-            url = users.create_logout_url(self.request.uri)
-            url_linktext = 'Logout'
-        else:
-            url = users.create_login_url(self.request.uri)
-            url_linktext = 'Login'
 
-        template_values = {
-            'greetings': greetings,
-            'guestbook_name': urllib.quote_plus(guestbook_name),
-            'url': url,
-            'url_linktext': url_linktext,
-        }
-
-        template = JINJA_ENVIRONMENT.get_template('index.html')
-        self.response.write(template.render(template_values))
-
-class Guestbook(webapp2.RequestHandler):
-
+class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):
     def post(self):
-        # We set the same parent key on the 'Greeting' to ensure each Greeting
-        # is in the same entity group. Queries across the single entity group
-        # will be consistent. However, the write rate to a single entity group
-        # should be limited to ~1/second.
-        guestbook_name = self.request.get('guestbook_name',
-                                          DEFAULT_GUESTBOOK_NAME)
-        greeting = Greeting(parent=guestbook_key(guestbook_name))
+        upload_files = self.get_uploads('file')  # 'file' is file upload field in the form
+        blob_info = upload_files[0]
+        user_file = UserFile(user=users.get_current_user().user_id(),
+                             file_name=blob_info.filename,
+                             blob_key=blob_info.key())
+        user_file.put()
+        self.redirect('/')
 
-        if users.get_current_user():
-            greeting.author = users.get_current_user()
 
-        greeting.content = self.request.get('content')
-        greeting.put()
-
-        query_params = {'guestbook_name': guestbook_name}
-        self.redirect('/?' + urllib.urlencode(query_params))
+class ServeHandler(blobstore_handlers.BlobstoreDownloadHandler):
+    def get(self, resource):
+        resource = str(urllib.unquote(resource))
+        blob_info = blobstore.BlobInfo.get(resource)
+        self.send_blob(blob_info, save_as=True)
 
 
 application = webapp2.WSGIApplication([
-    ('/', MainPage),
-    ('/sign', Guestbook),
-], debug=True)
+    ('/', MainHandler),
+    ('/upload', UploadHandler),
+    ('/serve/([^/]+)?', ServeHandler)], debug=True)
 
